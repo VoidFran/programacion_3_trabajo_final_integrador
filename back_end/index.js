88f38f60cdb7a229618eab6afe2a12c3ad4e3e45
VoidFran <franciscof2menosf1@gmail.com>
2023-10-05 23:02:05 -0300

    segunda entrega

--- back_end/index.js
+++ back_end/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // para poder trabajar con ellos en el manejdaor de la ruta.
 app.use(express.urlencoded({extended:true}))
 
-app.use(cors())
+app.use(cors());
 
 // Contacto
 app.post("/contacto", (req, res)=>{
@@ -36,6 +36,7 @@ app.post("/contacto", (req, res)=>{
     const cuerpo = `<body style= "background-color: #155fd9">
 	                    <h1 style="color: #f1c40f; font-size: 16px; margin: 16px">${titulo}</h1>
 	                        <p style="color: #ffffff; font-size: 16px; margin: 16px">${mensaje}</p>
+	                        <footer><p style="color: #ffffff; font-size: 16px; margin: 16px">Copyright© 2023 AFA / Todos los derechos reservados</p></footer>
                     </body>`;
 
     const opciones = {
@@ -59,5 +60,96 @@ app.post("/contacto", (req, res)=>{
 })
 
 app.listen(process.env.PUERTO, ()=>{
-    console.log("API prog3 iniciada " + process.env.PUERTO)
+    console.log("API prog3 iniciada en el puerto: " + process.env.PUERTO)
 })
+
+// -------------------------  BASE DE DATOS -----------------------------------------------------------
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database:"scalonetta"
+
+});
+
+app.post("/create",(req,res)=>{
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const camiseta = req.body.camiseta;
+    const pierna = req.body.pierna;
+    const posicion = req.body.posicion;
+
+
+    db.query('INSERT INTO jugador(nombre,edad,camiseta,pierna,posicion) VALUES(?,?,?,?,?)',[nombre,edad,camiseta,pierna,posicion],
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    }
+    );
+
+ });
+//  ----------------------------------------SOLICITA CON METODO GET A BASE DE DATOS UNA LISTA DE LOS JUGADORES--------------------------------------------XDD
+ app.get("/jugador",(req,res)=>{
+    db.query('SELECT * FROM jugador',
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    }
+    );
+
+ });
+
+
+
+
+ //  ----------------------------------------ACTUALIZAR JUGADORES--------------------------------------------XD
+
+ app.put("/update",(req,res)=>{
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const camiseta = req.body.camiseta;
+    const pierna = req.body.pierna;
+    const posicion = req.body.posicion;
+
+
+    db.query('UPDATE jugador SET nombre=?,edad=?,camiseta=?,pierna=?,posicion=? WHERE id=?',[nombre,edad,camiseta,pierna,posicion,id],
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    }
+    );
+
+ });
+
+ //  ----------------------------------------ELIMINAR JUGADORES--------------------------------------------XD
+
+
+
+
+
+ app.delete("/delete/:id",(req,res)=>{
+    const id = req.params.id;
+
+    db.query('DELETE FROM jugador WHERE id=?', id,
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    }
+    );
+
+ });
--- front_end/src/componentes/jugadores.js
+++ front_end/src/componentes/jugadores.js
@@ -1,7 +1,174 @@
+import { useState } from "react";
+import Axios from "axios";
+
 export default function Jugadores() {
-    return (
-        <div>
-            <h1>jugadores</h1>
-        </div>
-    )
+  const [nombre, setNombre] = useState("");
+  const [posicion, setPosicion] = useState("");
+  const [pierna, setPierna] = useState("");
+  const [edad, setEdad] = useState();
+  const [camiseta, setCamiseta] = useState();
+  const [jugadoresLista, setJugadores] = useState([]);
+  const [id, setId] = useState();
+  
+  const [editar, setEditar] = useState(false);
+
+  const add = () => {
+    if (nombre !== "" && edad !== "" && camiseta !== "" && pierna !== "" && posicion !== "") {
+      alert("Jugador agregado");
+      Axios.post("http://localhost:3005/create", {
+        nombre: nombre,
+        edad: edad,
+        camiseta: camiseta,
+        pierna: pierna,
+        posicion: posicion,
+      }).then(() => {
+        getJugador();
+        limpiar();
+      });
+    }
+    else {
+      alert("Jugador no agregado");
+    }
+  };
+
+  
+  const update = () => {
+    if (nombre !== "" && edad !== "" && camiseta !== "" && pierna !== "" && posicion !== "") {
+      alert("Jugador editado");
+      Axios.put("http://localhost:3005/update", {
+        id:id,
+        nombre: nombre,
+        edad: edad,
+        camiseta: camiseta,
+        pierna: pierna,
+        posicion: posicion,
+      }).then(() => {
+        getJugador();
+        limpiar();
+      });
+    }
+    else {
+      alert("Jugador no editado");
+    }
+  };
+
+  const eliminar = (id) => {
+    alert("Jugador eliminado");
+    Axios.delete(`http://localhost:3005/delete/${id}`, {
+    }).then(() => {
+      getJugador();
+      limpiar();
+    });
+  };
+
+
+
+  const editarJugador = (val)=>{
+    setEditar(true);
+    setNombre(val.nombre)
+    setEdad(val.edad)
+    setCamiseta(val.camiseta)
+    setPierna(val.pierna)
+    setPosicion(val.posicion)
+    setId(val.id)
+  
+  }
+
+  const limpiar = () => {
+    setEdad("");
+    setNombre("");
+    setCamiseta("");
+    setPosicion("");
+    setPierna("");
+    setId("");
+    setEditar(false);
+  }
+
+  const getJugador = () => {
+    Axios.get("http://localhost:3005/jugador").then((response) => {
+      setJugadores(response.data);
+    });
+  };
+  getJugador();
+  return (
+    <div class="container">
+    <div>
+      <div className="datos">
+        <label> Nombre y apellido :<input value={nombre} onChange={(event) => {
+              setNombre(event.target.value);
+            }}
+            type="text"/></label>
+        <label> Posicion de jugada:<input value={posicion} onChange={(event) => {
+              setPosicion(event.target.value);
+            }}
+            type="text"/></label>
+        <label>Pierna del jugador:<input value={pierna} onChange={(event) => {
+              setPierna(event.target.value);
+            }}type="text"
+          /></label>
+        <label>Edad del jugador :<input value={edad} onChange={(event) => {
+              setEdad(event.target.value);
+            }}
+            type="number"/></label>
+        <label>Numero de camiseta :<input value={camiseta} onChange={(event) => {
+              setCamiseta(event.target.value);
+            }}
+            type="number"/></label>
+          {
+            editar?
+            <div>
+            <button className="btn btn-warning m-2" onClick={update} style={{ marginTop: '20px' }}>Actualizar jugador</button> 
+            <button className="btn btn-info m-2 " onClick={limpiar} style={{ marginTop: '20px' }}>Cancelar</button>
+            </div>
+            : <button className="btn btn-success" onClick={add} style={{ marginTop: '20px' }}>Agregar jugador</button>
+           
+          }
+        
+
+      </div>
+    </div>
+    <table className="table table-striped" style={{ marginTop: '25px' }}>
+          
+        
+        <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre y apellido</th>
+          <th scope="col">Edad</th>
+          <th scope="col">Posicion</th>
+          <th scope="col" >Numero de camiseta</th>
+          <th scope="col">Pierna</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {jugadoresLista.map((val, key) => {
+            return <tr key ={val.id}>
+                    <th scope="row">{val.id}</th>
+                    <td>{val.nombre}</td>
+                    <td>{val.edad}</td>
+                    <td>{val.posicion}</td>
+                    <td>{val.camiseta}</td>
+                    <td>{val.pierna}</td>
+                    <td>
+                    <div className="btn-group" role="group" aria-label="Basic example">
+                      <button type="button"
+                      onClick={()=>{
+                        editarJugador(val);
+                      }}
+                      className="btn btn-info">Editar</button>
+                      <button type="button" onClick={() =>{eliminar(val.id)}} className="btn btn-danger">Eliminar</button>
+                      
+                    </div>
+
+                    </td>
+                    </tr>
+
+          })}
+       
+      </tbody>
+    </table>
+
+    </div>
+  );
 }
